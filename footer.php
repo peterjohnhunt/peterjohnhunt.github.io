701d2083032a3d192debad40e9864cbbad337c1f
peterjohnhunt <github@peterjohnhunt.com>
2015-07-20 14:25:41 -0500

    Removed Un-needed PHP files

--- footer.php
+++
@@ -1,9 +0,0 @@
-	<div class="footer">
-		&copy; <?php echo date('Y'); ?> PeterJohn Hunt. All Rights Reserved.
-	</div>
-	<!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script type="text/javascript" src="js/jquery.mobile.js"></script>
-	<script type="text/javascript" src="js/functions.js"></script> -->
-</body>
-
-</html>
